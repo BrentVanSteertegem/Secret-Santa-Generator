1b63ca43c9c3af2dfe28b71dcec5155c0d3ace7b
Brent Van Steertegem <brent.vansteertegem@gmail.com>
2024-10-10 22:16:27 +0200

    feat: Added icons library and made it possible to delete people

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,19 @@ const people = ref([])
 function addPerson(person) {
   people.value.push(person)
 }
+
+function removePerson(index) {
+  people.value.splice(index, 1)
+}
 </script>
 
 <template>
-  <div class="bg-green-950 text-neutral-50 w-screen h-screen p-4">
+  <div class="bg-green-950 text-neutral-50 w-screen h-screen p-4 flex flex-col gap-8">
     <AddForm @addPerson="addPerson" />
-    <ul v-for="(person, i) in people" :key="i">
-      <PersonCard :name="person.name" />
+    <ul class="flex flex-wrap justify-center gap-2">
+      <li v-for="(person, i) in people" :key="i">
+        <PersonCard :name="person.name" @removePerson="() => removePerson(i)"/>
+      </li>
     </ul>
   </div>
 </template>
--- src/components/PersonCard.vue
+++ src/components/PersonCard.vue
@@ -2,8 +2,21 @@
 const props = defineProps({
     name: String
 })
+
+const emit = defineEmits(['removePerson'])
+
+function onClick($event) {
+    emit('removePerson', $event)
+}
 </script>
 
 <template>
-    <li>{{ props.name }}</li>
+    <section class="bg-red-800 p-2 rounded-lg w-60 justify-between">
+        <p>
+            {{ props.name }}
+        </p>
+        <button @click="onClick">
+            <font-awesome-icon :icon="['far', 'trash-can']" />
+        </button>
+    </section>
 </template>
