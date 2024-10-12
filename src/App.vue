bbadb903c9e4e84e9851288bcd377f54913c7120
Brent Van Steertegem <brent.vansteertegem@gmail.com>
2024-10-12 14:14:28 +0200

    feat: Added jumpscare

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,51 @@
 <script setup>
+import { ref } from 'vue'
 import './styles/index.css'
 import AddForm from './components/AddForm.vue'
-import { ref } from 'vue'
 import PersonCard from './components/PersonCard.vue'
 
 const people = ref([])
 
 function addPerson(person) {
+  if(!$overlay) setOverlay()
   people.value.push(person)
 }
 
 function removePerson(index) {
   people.value.splice(index, 1)
 }
+
+let $overlay
+function setOverlay() {
+  $overlay = document.querySelector('.c-overlay')
+}
+
+function jumpScare() {
+  const randomTimeout = Math.floor(Math.random() * 2000) + 1000
+  const randomDisplayTime = Math.floor(Math.random() * 10) + 100
+  setTimeout(() => {
+    $overlay.classList.remove('hidden')
+    setTimeout(() => {
+      $overlay.classList.add('hidden')
+    }, randomDisplayTime)
+  }, randomTimeout)
+}
 </script>
 
 <template>
   <div class="bg-green-950 text-neutral-50 w-screen h-screen p-4 flex flex-col gap-8">
-    <AddForm @addPerson="addPerson" />
+    <AddForm @addPerson="addPerson" @jumpscare="jumpScare"/>
+    <div class="c-overlay hidden absolute h-screen w-screen top-0 left-0"/>
     <ul class="flex flex-wrap justify-center gap-2">
       <li v-for="(person, i) in people" :key="i">
-        <PersonCard :name="person.name" @removePerson="() => removePerson(i)"/>
+        <PersonCard :name="person.name" @removePerson="() => removePerson(i)" @selectPerson="selectPerson"/>
       </li>
     </ul>
   </div>
 </template>
+
+<style>
+  .c-overlay {
+    background: url('./assets/jelle.png') no-repeat center center;
+  }
+</style>
--- src/components/AddForm.vue
+++ src/components/AddForm.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { ref } from 'vue'
 
-const emit = defineEmits(['addPerson'])
+const emit = defineEmits(['addPerson', 'jumpscare'])
 
 const name = ref('')
 
 function addPerson() {
   emit('addPerson', { name: name.value })
+  if (name.value.trim().toLowerCase() === 'jelle') {
+    emit('jumpscare')
+  }
   name.value = ''
 }
 
@@ -21,7 +24,7 @@ function listenForChange($event) {
   <div class="flex justify-center bg-inherit">
     <div class="flex flex-col w-60 gap-2">
       <input type="text" v-model="name" @keypress="listenForChange"
-        class="border border-white bg-inherit p-2 rounded-lg text-center" />
+        class="border border-white bg-inherit p-2 rounded-lg text-center focus:outline-none" />
       <button @click="addPerson" class="border border-white p-2 rounded-lg">Add</button>
     </div>
   </div>
