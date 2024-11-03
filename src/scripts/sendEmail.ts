50139e797d465d97c82c169c30c87111b6ed2460
Brent Van Steertegem <brent.vansteertegem@gmail.com>
2024-11-03 17:16:41 +0100

    feat: Capitalized names

--- src/scripts/sendEmail.ts
+++ src/scripts/sendEmail.ts
@@ -2,9 +2,9 @@ import emailjs from 'emailjs-com'
 
 export function sendEmail(from: any, to: any) {
     emailjs.send(process.env.VUE_APP_EMAIL_SERVICE_ID, process.env.VUE_APP_EMAIL_TEMPLATE_ID, {
-    from_name: from.name,
+    from_name: from.name[0].toUpperCase() + from.name.slice(1),
     from_email: from.email,
-    to_name: to.name,
+    to_name: to.name[0].toUpperCase() + to.name.slice(1),
     }, process.env.VUE_APP_EMAIL_USER_ID)
     .then((response) => {
         console.log('Email successfully sent!', response.status, response.text)
